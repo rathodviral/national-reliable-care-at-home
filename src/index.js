10de1e4e6c524ad301ba1fef91440a135080a907
Viral Rathod <rvdrathod.viral1989@gmail.com>
2023-06-17 10:25:43 +0530

    new domain

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={"national-reliable-care-at-home"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
